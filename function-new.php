8eee039c8c84afacf2994ee8efe32daf64c0c570
Michael Kraus <michaelkraus@Booger-Hook-on-the-Bang-Switch.local>
2019-10-02 09:41:35 -0700

    Added files and access to DB

--- function-new.php
+++ function-new.php
@@ -1,11 +1,11 @@
 <?php
 
 function makeNav($conn){
-    $sql = "SELECT pagename FROM test.content";
+    $sql = "SELECT pagename, pagetitle FROM test.navigation";
     $result = $conn->query($sql);
     echo "<ul>";
     while ( $row = $result->fetch_assoc() ) {
-        echo "<li><a href='index.php?page=" . $row['pagename'] . "'>" .$row['pagename']. "</a></li>";
+        echo "<li><a href='" . $row['pagename'] . "'>" .$row['pagetitle']. "</a></li>";
     }
     echo "</ul>";
 }
@@ -17,4 +17,11 @@ function makeNav($conn){
         echo "<p>" . $row['pagetitle'] . "</p>";
         echo "<p>" . $row['pagename'] . "</p>";
         echo $row['pagecontent'];
+    }
+
+    function makeTitle($conn, $thisPagename){
+        $sql = "SELECT * FROM test.content WHERE pagename = '$thisPagename'";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        echo $row['pagename'];
     }
--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $thisPagename = $_GET["page"] ?? "Home";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php ?></title>
 </head>
   <body>
     <nav>
@@ -21,7 +21,7 @@ $thisPagename = $_GET["page"] ?? "Home";
     <section>
       <div>
         <?php
-        makeContent($conn, $thisPagename);
+        // makeContent($conn, $thisPagename);
 
         // echo "test" . $thisPagename;
 
